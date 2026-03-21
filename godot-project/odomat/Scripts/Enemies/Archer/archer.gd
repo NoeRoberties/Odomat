2827d45c19e8fa7ae7ff36cbfc43c3d41253cde4
Cédric Khambounheuang <cedric.khambounheuang@epitech.eu>
2026-03-21 23:35:07 +0100

    [UPT] Fixed coding style + cleared code

--- godot-project/odomat/Scripts/Enemies/Archer/archer.gd
+++ godot-project/odomat/Scripts/Enemies/Archer/archer.gd
@@ -2,56 +2,90 @@ extends CharacterBody2D
 
 @export var arrow_scene : PackedScene = preload("res://Scenes/Enemies/Archer/arrow.tscn")
 
-var speed = 90
-var flee_speed = 130
-var stop_distance = 250.0
+var _speed = 90
+var _flee_speed = 130
+var _shoot_distance = 250.0
 
-var player = null
-var in_attack_zone = false
-var in_danger_zone = false
+var _player = null
+var _in_attack_zone = false
+var _in_danger_zone = false
 
-var shoot_cooldown = 2.0
-var shoot_timer = 0.0
+var _shoot_cooldown = 1.0
+var _shoot_timer = 0.0
+var _is_shooting = false
 
 
 func _physics_process(delta: float) -> void:
-	if player == null:
-		velocity = Vector2.ZERO
-		$AnimatedSprite2D.play("idle")
+	if _is_shooting:
+		move_and_slide()
+		return
+		
+	if _player == null:
+		_idle()
 		move_and_slide()
 		return
 
-	if in_danger_zone:
-		var flee_dir = (global_position - player.global_position).normalized()
-		velocity = flee_dir * flee_speed
-		$AnimatedSprite2D.play("walk")
+	if _in_danger_zone:
+		_flee()
+	elif _in_attack_zone:
+		_attack(delta)
 
-	elif in_attack_zone:
-		var dist = global_position.distance_to(player.global_position)
-		if dist > stop_distance:
-			var approach_dir = (player.global_position - global_position).normalized()
-			velocity = approach_dir * speed
-			$AnimatedSprite2D.play("walk")
-		else:
-			velocity = Vector2.ZERO
-			if $AnimatedSprite2D.animation != "shoot":
-				$AnimatedSprite2D.play("idle")
-			shoot_timer -= delta
-			if shoot_timer <= 0.0:
-				shoot()
-				shoot_timer = shoot_cooldown
-
-	if in_danger_zone:
-		$AnimatedSprite2D.flip_h = (player.position.x > position.x)
-	else:
-		$AnimatedSprite2D.flip_h = (player.position.x < position.x)
-
+	_update_sprite_direction()
 	move_and_slide()
 
-func shoot() -> void:
-	if not arrow_scene or not player:
+
+func _idle() -> void:
+	velocity = Vector2.ZERO
+	$AnimatedSprite2D.play("idle")
+
+
+func _flee() -> void:
+	var flee_dir = (global_position - _player.global_position).normalized()
+	velocity = flee_dir * _flee_speed
+	$AnimatedSprite2D.play("walk")
+
+
+func _attack(delta: float) -> void:
+	var dist = global_position.distance_to(_player.global_position)
+	if dist > _shoot_distance:
+		_approach()
+	else:
+		_try_shoot(delta)
+
+
+func _approach() -> void:
+	var approach_dir = (_player.global_position - global_position).normalized()
+	velocity = approach_dir * _speed
+	$AnimatedSprite2D.play("walk")
+
+
+func _try_shoot(delta: float) -> void:
+	velocity = Vector2.ZERO
+	if not _is_shooting:
+		$AnimatedSprite2D.play("idle")
+	_shoot_timer -= delta
+	if _shoot_timer <= 0.0:
+		_shoot()
+		_shoot_timer = _shoot_cooldown
+
+
+func _update_sprite_direction() -> void:
+	if _in_danger_zone:
+		$AnimatedSprite2D.flip_h = (_player.position.x > position.x)
+	else:
+		$AnimatedSprite2D.flip_h = (_player.position.x < position.x)
+
+
+func _shoot() -> void:
+	if not arrow_scene or not _player:
 		return
+
+	_is_shooting = true
+	var target_position = _player.global_position
 	$AnimatedSprite2D.play("shoot")
+
+	var shoot_duration = $AnimatedSprite2D.sprite_frames.get_frame_count("shoot") / $AnimatedSprite2D.sprite_frames.get_animation_speed("shoot")
+	await get_tree().create_timer(shoot_duration - 0.5).timeout
 
 	var offset = Vector2(5, -30)
 	if $AnimatedSprite2D.flip_h:
@@ -61,38 +95,49 @@ func shoot() -> void:
 
 	var arrow = arrow_scene.instantiate()
 	arrow.pos = shoot_pos
-	arrow.dir = (player.global_position - shoot_pos).angle()
+	arrow.dir = (target_position - shoot_pos).angle()
 	arrow.rota = arrow.dir
-	arrow.target = player
+	arrow.target = _player
 	get_tree().current_scene.add_child(arrow)
+	
+	_is_shooting = false
+	$AnimatedSprite2D.play("idle")
+
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if $AnimatedSprite2D.animation == "shoot":
+		_is_shooting = false
 		$AnimatedSprite2D.play("idle")
+
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("player"):
 		return
-	player = body
-	in_attack_zone = true
-	shoot_timer = shoot_cooldown
+	_player = body
+	_in_attack_zone = true
+	_shoot_timer = _shoot_cooldown
+
 
 func _on_attack_area_body_exited(body: Node2D) -> void:
 	if not body.is_in_group("player"):
 		return
-	in_attack_zone = false
-	if not in_danger_zone:
-		player = null
+	_in_attack_zone = false
+	_is_shooting = false
+	_shoot_timer = _shoot_cooldown
+	if not _in_danger_zone:
+		_player = null
+
 
 func _on_danger_area_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("player"):
 		return
-	player = body
-	in_danger_zone = true
+	_player = body
+	_in_danger_zone = true
+
 
 func _on_danger_area_body_exited(body: Node2D) -> void:
 	if not body.is_in_group("player"):
 		return
-	in_danger_zone = false
-	if not in_attack_zone:
-		player = null
+	_in_danger_zone = false
+	if not _in_attack_zone:
+		_player = null
