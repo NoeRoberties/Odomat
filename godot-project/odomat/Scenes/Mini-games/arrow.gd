8b7ab018378b10c5c46aca87097271dcb3c4ce75
Cédric Khambounheuang <khambou@Mac.lan>
2026-07-19 12:55:44 +0200

    [EDIT] : new game, new directory, new sounds

--- godot-project/odomat/Scenes/Mini-games/arrow.gd
+++ godot-project/odomat/Scenes/Mini-games/arrow.gd
@@ -1,24 +1,22 @@
 extends Area2D
 class_name Arrow
 
-@export var speed: float = 400.0
+var speed: float = 200.0
+var hit_size: float = 60.0
 var direction: Vector2 = Vector2.ZERO
 var arrow_type: String = ""
 
 func _ready() -> void:
-	pass
+	if $CollisionShape2D.shape == null:
+		$CollisionShape2D.shape = RectangleShape2D.new()
+	($CollisionShape2D.shape as RectangleShape2D).size = Vector2(hit_size, hit_size)
 
 func set_direction(new_direction: Vector2) -> void:
 	direction = new_direction
-	
-	if direction == Vector2.RIGHT:
-		rotation_degrees = 90
-	elif direction == Vector2.LEFT:
-		rotation_degrees = -90
-	elif direction == Vector2.DOWN:
-		rotation_degrees = 180
-	elif direction == Vector2.UP:
-		rotation_degrees = 0
+	rotation = direction.angle()
 
 func _process(delta: float) -> void:
 	position += direction * speed * delta
+	var vp = get_viewport_rect().size
+	if position.x < -400 or position.x > vp.x + 400 or position.y < -400 or position.y > vp.y + 400:
+		queue_free()
